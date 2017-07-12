400f3992a104ebf3c81e60931b684b893c9a314a
Szirmai Gyozo <szirm.gyozo@gmail.com>
2017-07-12 15:48:14 +0200

    wp-config db-host:db change

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define('DB_USER', 'wordpress');
 define('DB_PASSWORD', 'nemtudom');
 
 /** MySQL hostname */
-define('DB_HOST', 'db_wordpress');
+define('DB_HOST', 'db');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
